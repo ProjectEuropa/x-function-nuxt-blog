3121728b3675989548187a561d9857079451e9fc
M2 <project-europa@excite.co.jp>
2018-05-16 12:58:09 +0900

    feat: footer作成

---
+++ components/TheFooter/TheFooter.vue
@@ -0,0 +1,29 @@
+<template>
+    <footer>
+      <small>{{ copyrights }}</small>
+    </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      copyrights: new Date().getFullYear() + " Team Project Europa"
+    };
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+footer {
+  position: fixed;
+  bottom: 0;
+  background: #022d30;
+  width: 100%;
+  height: 30px;
+  text-align: center;
+  color: #ffffff;
+}
+</style>
+
