37e2494e152b144bc31a61908fc8fdc1062eae5c
M2 <project-europa@excite.co.jp>
2018-05-20 17:53:55 +0900

    feat: top画面にgifアニメーションを追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,23 @@
 <template>
   <section id="posts">
-    <post-preview
-     v-for="post in posts"
-     :key="post.id"
-     :title="post.title"
-     :excerpt="post.previewText"
-     :thumbnailImage="post.thumbnailUrl"
-     :id="post.id"
-    />
+    	<div class="title-container">
+        <h1>Welcome to Blog of Team Project Europa.</h1>
+        <p class="message">We love CarnageHeart series.</p>
+        <div class="gif">
+          <img src="@/assets/image/mocking.gif">
+          <img src="@/assets/image/tuki.gif">
+        </div>
+      </div>
   </section>
 </template>
 
 <script>
-import PostPreview from "@/components/Blog/PostPreview";
-
 export default {
-  components: {
-    PostPreview
-  },
-  asyncData(context) {
-    return context.app.$storyapi
-      .get("cdn/stories", {
-        version: context.isDev ? "draft" : "published",
-        starts_with: "blog/"
-      })
-      .then(res => {
-        return {
-          posts: res.data.stories.map(bp => {
-            return {
-              id: bp.slug,
-              title: bp.content.title,
-              previewText: bp.content.summary,
-              thumbnailUrl: bp.content.thumbnail
-            };
-          })
-        };
-      });
-  }
-  // data() {
-  //   return {
-  //     posts: [
-  //       {
-  //         title: 'A new Begining',
-  //         previewText: 'This will be awesome , don\'t miss it!',
-  //         thumbnailUrl: 'https://www.healthyfood.co.uk/wp-content/themes/healthyfoodguide/assets/img/structure/hfg-logo-022017.png',
-  //         id: 'a-new-beginning'
-  //       },
-  //       {
-  //         title: 'A Second Begining',
-  //         previewText: 'This will be awesome , don\'t miss it!',
-  //         thumbnailUrl: 'https://www.healthyfood.co.uk/wp-content/themes/healthyfoodguide/assets/img/structure/hfg-logo-022017.png',
-  //         id: 'a-second-beginning'
-  //       },
-  //     ]
-  //   }
-  // }
+  components: {}
 };
 </script>
 
-<style>
+<style lang="css">
 #posts {
   padding-top: 2rem;
   display: flex;
@@ -79,6 +38,89 @@ body {
 @media (min-width: 35rem) {
   #posts {
     flex-direction: row;
+  }
+}
+</style>
+
+<style lang="scss" scoped>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Roboto", serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: #dedede;
+}
+
+h1,
+p ,
+.gif{
+  position: relative;
+  display: block;
+  margin: 0;
+  font-size: 4rem;
+  line-height: 1;
+  transform: translateY(6rem);
+  animation: up 500ms linear forwards;
+  z-index: 1;
+  text-shadow: 0px 1px 1px rgba(255, 255, 255, 1);
+
+  &::before,
+  &::after {
+    position: absolute;
+    content: "";
+    width: 0px;
+    height: 1px;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.2);
+    z-index: -1;
+  }
+
+  &::before {
+    top: 1.4rem;
+  }
+
+  &::after {
+    bottom: 0.4rem;
+  }
+}
+
+.title-container {
+  font-size: 0;
+  position: relative;
+  overflow: hidden;
+  padding-bottom: 0.4rem;
+  .message {
+    text-align: center;
+  }
+  .gif {
+    text-align: center;
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-evenly;
+  }
+}
+
+@keyframes up {
+  100% {
+    transform: translateY(0);
+  }
+}
+
+@keyframes draw {
+  100% {
+    width: 1%;
   }
 }
 </style>
