dd002811975a5c8a7a019a945a45f96f4965bf47
宮本雅人 <masato@miyamotomasatonoMacBook-puro.local>
2018-06-13 15:12:32 +0900

    feat: フッターにTwitterとGitHubのアイコンを表示

--- components/TheFooter/TheFooter.vue
+++ components/TheFooter/TheFooter.vue
@@ -1,5 +1,8 @@
 <template>
     <footer>
+      <p><a href="https://twitter.com/M2_m_" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+        <a href="https://github.com/ProjectEuropa" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
+      </p>
       <small>{{ copyrights }}</small>
     </footer>
 </template>
@@ -8,7 +11,7 @@
 export default {
   data() {
     return {
-      copyrights: new Date().getFullYear() + " Team Project Europa"
+      copyrights: '© ' + new Date().getFullYear() + " Team Project Europa"
     };
   }
 };
@@ -17,13 +20,23 @@ export default {
 
 <style lang="scss" scoped>
 footer {
-  padding-top: 3rem;
+  padding-top: 2rem;
   bottom: 0;
   background: #1A1A1A;
   width: 100%;
-  height: 6rem;
+  height: 8rem;
   text-align: center;
   color: #ffffff;
+  .fa {
+    font-size: 1.5em;
+    margin: 0 10px;
+  }
+  .fa-twitter {
+    color: #00aced;
+  }
+  .fa-github {
+    color: #ffffff;
+  }
 }
 </style>
 
